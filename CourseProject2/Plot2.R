4390c968b2125de17c4b9bfe6d131ff752114491
Anil Kumar Khadka <akhadka525@gmail.com>
2023-11-01 21:32:30 -0400

    Code updated for plo2 problem and plot2 created

---
+++ CourseProject2/Plot2.R
@@ -0,0 +1,70 @@
+## Load the necessary libraries. 
+## If not installed, install and load the required libraries
+
+packages <- c("dplyr")
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+        install.packages(packages[!installed_packages])
+}
+invisible(lapply(packages, library, character.only = TRUE))
+
+## Get the path of current working directory
+dir_path <- getwd() 
+
+## Download the data if not downlaoded and load it in memory
+## Create data url and path to unzip data and download the data
+data_url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+zip_data_file <- "NEI_data.zip"
+unzip_dir <- "data"
+
+
+# Check if the file already exists in the current directory then download data
+if (!file.exists(zip_data_file)) {
+        download.file(data_url, zip_data_file) # If the file does not exist, download it
+        print("File downloaded successfully.")
+} else {
+        print("File already exists.")
+}
+
+# Check if the unzipped data folder already exists
+
+if (!dir.exists(unzip_dir)){
+        unzip(zip_data_file, exdir = unzip_dir) # If the folder does not exist, unzip the file
+        print("Data unzipped successfully.")
+} else {
+        print("Data folder already exists.")
+}
+
+
+NEI <- readRDS("data/summarySCC_PM25.rds")
+SCC <- readRDS("data/Source_Classification_Code.rds")
+
+## For Baltimore, Maryland fips = 24510.
+## Filter the data by required fips, then select the data and calculate total Pm2.5 emissions
+## from all sources grouping by year
+
+total_emission_Baltimore <- NEI %>%
+        filter(fips == 24510) %>%
+        select(fips, Emissions, year) %>%
+        group_by(year) %>%
+        summarise(totalEmission = sum(Emissions, na.rm = TRUE))
+
+## Now Plot the total Pm2.5 emissions for Baltimore from all sources for each year and save to plot2.png
+
+{
+        png("Plot2.png", width=720, height=720)
+        
+        par(mgp = c(2.2, 1, 0))
+        barplot(total_emission_Baltimore$totalEmission/10^3,
+                names.arg = total_emission_Baltimore$year,
+                ## emission value is large so expressed in scale of 10^3 
+                col = "Blue",
+                xlab = "Year",
+                ylab = expression(Total ~ PM[2.5] ~ Emissions ~ (10^3 ~ Tons), line = 0),
+                main = expression(Total ~ PM[2.5] ~ Emissions ~ From ~ All ~ Sources ~
+                                          For ~ Baltimore ~ City ~ Maryland),
+                ylim = c(0, max(total_emission_Baltimore$totalEmission/10^3+1)))
+                ## setting Y-axis limit
+        
+        dev.off()
+        }
